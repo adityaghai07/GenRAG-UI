30fbe4f49850e673378546d3a52104c835678a28
Pratik <pratiksolanki2021@gmail.com>
2024-07-31 18:09:41 +0530

    added metrics display functionality

--- frontend/src/components/textloader.jsx
+++ frontend/src/components/textloader.jsx
@@ -1,7 +1,7 @@
 const TextLoader = () => {
   return (
     <div className="flex items-center">
-      <div className="text-base text-[#444444]">Generating response</div>
+      <div className="text-base text-[#444444] mt-2">Generating response</div>
       <div className="loading loading03 flex gap-2 text-[#444444] pl-4">
         <span>.</span>
         <span>.</span>
